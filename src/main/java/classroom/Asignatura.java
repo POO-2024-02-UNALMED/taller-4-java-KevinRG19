6eedb3fcf3cc3c8066140e925199a457cf63cf92
KevinRG19 <kerubianog@unal.edu.co>
2024-11-25 12:59:26 -0500

    Intento3

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -1,5 +1,4 @@
 package classroom;
-
 public class Asignatura {
 
     public String nombre;
@@ -10,11 +9,6 @@ public class Asignatura {
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
-    }
-
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
     }
 
     public Asignatura(int codigoExterno,Tipo tipo) {
@@ -39,10 +33,6 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
-
     public void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
@@ -54,7 +44,7 @@ public class Asignatura {
     public void setCodigoInterno(int codigoInterno) {
         this.codigoInterno = codigoInterno;
     }
-    
+
     public void setCodigoInterno(double codigoInterno) {
         this.codigoInterno = (int) codigoInterno;
     }
